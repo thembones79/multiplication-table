08345f457d2f053d0db91b38d62cacf7cbc395dd
thembones79 <michal.szumnarski@gmail.com>
2023-01-12 22:04:52 +0100

    added

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,5 @@
-import { getStorage, setStorage } from "../utils/storage";
+import { create } from "../utils/storage";
 
-const USER_NAME = "USER_NAME";
-export const getUserName = () => getStorage(USER_NAME);
-export const saveUserName = (name: string) => setStorage(USER_NAME, name);
-
-PICKED_MULTIPLIERS;
+export const data = {
+  userName: create<string>("USER_NAME"),
+};
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,14 @@
-export const getStorage = (key: string) =>
-  JSON.parse(localStorage.getItem(key) || "");
+export function getStorage<T>(key: string): T {
+  return JSON.parse(localStorage.getItem(key) || "");
+}
 
-export const setStorage = (key: string, value: Object) =>
+export function setStorage<T>(key: string, value: T) {
   localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function create<T>(key: string) {
+  return {
+    get: () => getStorage<T>(key),
+    set: (value: T) => setStorage<T>(key, value),
+  };
+}
